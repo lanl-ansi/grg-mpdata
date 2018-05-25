b2be971d8b5ca33da550e7d1221063dcc8e745c7
Carleton Coffrin <carleton@coffr.in>
2018-05-25 13:04:17 -0600

    Setup python version to pull from git tag (#9)
    * add test to check_version
    * adding pypi to travis

--- grg_mpdata/__init__.py
+++ grg_mpdata/__init__.py
@@ -1,8 +1,8 @@
 """a package for reading and writing of matpower data files"""
 
+__version__ = '0.0.1'
+
 # import standard entry points to the code
 from grg_mpdata import io
 from grg_mpdata import exception
 from grg_mpdata import cmd
-
-__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -5,16 +5,11 @@ import re
 
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-def read(*parts):
-    # intentionally *not* adding an encoding option to open
-    return codecs.open(os.path.join(here, *parts), 'r').read()
-
 def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    with open(os.path.join(os.path.dirname(__file__), *file_paths)) as file:
+        version_file = file.read()
+
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
