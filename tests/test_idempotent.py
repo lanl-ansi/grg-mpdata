f232dbeece28f917c9c34333a64e5eddd8557028
carleton <cjc@lanl.gov>
2018-05-25 17:34:08 -0600

    adding diff count test to idempotent testing

--- tests/test_idempotent.py
+++ tests/test_idempotent.py
@@ -9,9 +9,6 @@ from test_common import warning_files
 #@pytest.mark.filterwarnings('error')
 @pytest.mark.parametrize('input_data', correct_files)
 def test_001(input_data):
-    # lets be extra strick and flag warnings here as well
-    #warnings.simplefilter('error')
-
     case = grg_mpdata.io.parse_mp_case_file(input_data)
     mp_data = case.to_matpower()
     case_2 = grg_mpdata.io.parse_mp_case_str(mp_data)
@@ -19,17 +16,13 @@ def test_001(input_data):
     assert not case != case_2
     assert str(case) == str(case_2) # checks string representation of data structure
 
-    # need to set warnings back to default
-    # otherwise tests using pytest.warns will fail
-    #warnings.resetwarnings()
-    #warnings.filterwarnings('always')
+    diff_count = grg_mpdata.cmd.diff(case, case_2)
+    assert diff_count <= 0
 
 
 #@pytest.mark.filterwarnings('default')
 @pytest.mark.parametrize('input_data', warning_files)
 def test_002(input_data):
-    #warnings.simplefilter('default')
-
     case = grg_mpdata.io.parse_mp_case_file(input_data)
     mp_data = case.to_matpower()
     case_2 = grg_mpdata.io.parse_mp_case_str(mp_data)
@@ -37,4 +30,5 @@ def test_002(input_data):
     assert not case != case_2
     assert str(case) == str(case_2) # checks string representation of data structure
 
-    #warnings.filterwarnings('always')
+    diff_count = grg_mpdata.cmd.diff(case, case_2)
+    assert diff_count <= 0
