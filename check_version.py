5c6314a3f8980dbfefcbbb35e9f89c196e3728a2
carleton <cjc@lanl.gov>
2018-05-25 11:06:29 -0600

    add test to check_version

--- check_version.py
+++ check_version.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
+import sys
 import subprocess
 import grg_mpdata
 
-git_version = subprocess.check_output(['git', 'describe', '--tags']).rstrip().decode('ascii')
+git_describe = subprocess.check_output(['git', 'describe', '--tags']).rstrip().decode('ascii')
 
 py_version = grg_mpdata.__version__
 
-print(git_version)
-print(py_version)
+print('git describe: {}'.format(git_describe))
 
+git_version = git_describe.split('-')[0].strip('v')
+print('git version: {}'.format(git_version))
+
+print('grg_mpdata version: {}'.format(py_version))
+
+
+if git_version != py_version:
+    print('git and python versions do not match')
+    sys.exit(1)
